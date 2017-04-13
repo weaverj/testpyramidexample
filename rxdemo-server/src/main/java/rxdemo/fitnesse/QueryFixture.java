c1038f384b8f5d0208be3e0aadd52b7aac6c9702
Jim Weaver <james.weaver@vanderbilt.edu>
2017-04-13 09:50:02 -0500

    initial fitnesse test, not passing yet

---
+++ rxdemo-server/src/main/java/rxdemo/fitnesse/QueryFixture.java
@@ -0,0 +1,14 @@
+package rxdemo.fitnesse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryFixture {
+
+    protected List<String> buildColumn(String columnName, String value) {
+        ArrayList<String> column = new ArrayList<>();
+        column.add(columnName);
+        column.add(value);
+        return column;
+    }
+}
---
+++ rxdemo-server/src/main/java/rxdemo/fitnesse/TherapeuticClassesConsideredOpiates.java
@@ -0,0 +1,12 @@
+package rxdemo.fitnesse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TherapeuticClassesConsideredOpiates extends QueryFixture {
+
+    public List<List<List<String>>> query() {
+        ArrayList<List<List<String>>> tableList = new ArrayList<>();
+        return tableList;
+    }
+}
