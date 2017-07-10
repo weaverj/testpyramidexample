65fadba97ffaa0891cb770b4d6ab4cf7cf6d8904
Jim Weaver <james.weaver@vanderbilt.edu>
2017-07-10 15:15:27 -0500

    switching to PhantomJS for unit tests

--- rxdemo-ui/karma.conf.js
+++ rxdemo-ui/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: false,
     // client.args must be a array of string.
     // Leave 'aurelia-root', project.paths.root in this order so we can find
