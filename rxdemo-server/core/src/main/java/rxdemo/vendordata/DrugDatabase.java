98a1ab478c19f898def1c11a826e6cd7dc83d609
weaverj <weaver.je@gmail.com>
2017-09-08 23:07:57 -0500

    Adding drug for test

--- rxdemo-server/core/src/main/java/rxdemo/vendordata/DrugDatabase.java
+++ rxdemo-server/core/src/main/java/rxdemo/vendordata/DrugDatabase.java
@@ -66,6 +66,8 @@ public class DrugDatabase {
          new EDrugClassification[]{EDrugClassification.ANALGESICS_NARCOTIC}));
       drugsInDatabase.add(new DispensableDrug(1000, "simvastatin",
          new EDrugClassification[]{EDrugClassification.ANTIHYPERLIPIDEMICS}));
+
+      drugsInDatabase.add(new DispensableDrug(1100, "cowbell", new EDrugClassification[] {EDrugClassification.ANALGESIC}));
    }
 
 
